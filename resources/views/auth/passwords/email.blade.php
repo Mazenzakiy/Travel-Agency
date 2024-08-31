45190f07547d1b445d4e1b273315fea2ad88e4e3
unknown <m@gmail.com>
2024-08-31 15:03:07 +0300

    upload the project

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -43,5 +43,49 @@
             </div>
         </div>
     </div>
+</div> --}}
+
+
+
+
+<div class="reservation-form" style="margin-top: 181px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+                <form id="reservation-form" method="POST" role="search" action="{{ route('password.email') }}">
+                    @csrf
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h4>Reset Password</h4>
+                        </div>
+                        <div class="col-md-12">
+                            <fieldset>
+                                <label for="Name" class="form-label">E-Mail Address</label>
+                                <input id="email" placeholder="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </fieldset>
+                        </div>
+
+                        <div class="col-lg-12">
+                            <fieldset>
+                                <button type="submit" class="main-button">Send Password Reset Link</button>
+                            </fieldset>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
+
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,60 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+<div class="reservation-form" style="margin-top: 181px;">
+    <div class="container">
+        <div class="row">
 
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+            <div class="col-lg-12">
+                <form id="reservation-form" method="POST" role="search" action="{{ route('password.update') }}">
+                    <input type="hidden" name="token" value="{{ $token }}">
+                    @csrf
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h4>Reset Password</h4>
+                        </div>
+                        <div class="col-md-12">
+                            <fieldset>
+                                <label for="Name" class="form-label">E-Mail Address</label>
+                                <input id="email" placeholder="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
+                            </fieldset>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                        <div class="col-md-12">
+                            <fieldset>
+                                <label for="Name" class="form-label">Password</label>
+                                <input id="password" placeholder="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
+                            </fieldset>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                        <div class="col-md-12">
+                            <fieldset>
+                                <label for="Name" class="form-label">Confirm Password</label>
+                                <input id="password-confirm" placeholder="Confirm Password" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </fieldset>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
+                        <div class="col-lg-12">
+                            <fieldset>
+                                <button type="submit" class="main-button">Reset Password</button>
+                            </fieldset>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
